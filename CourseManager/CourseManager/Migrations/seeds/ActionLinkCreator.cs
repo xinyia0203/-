1ecb1b3022ee23b3705e220cb8ad997515596181
xinyia0203 <397882880@qq.com>
2019-06-29 08:35:33 +0800

    创建项目

---
+++ CourseManager/CourseManager/Migrations/seeds/ActionLinkCreator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CourseManager.Migrations.seeds
+{
+    public class ActionLinkCreator : Controller
+    {
+        //
+        // GET: /ActionLinkCreator/
+        private CourseManager.Models.CourseManagerEntities _context;
+
+        public ActionResult Index(CourseManager.Models.CourseManagerEntities context)
+        {
+            return View();
+        }
+
+    }
+}
